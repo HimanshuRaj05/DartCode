f3d6e57d2a3345a9b0425e13784a4dbec69c71fd
Himanshu <charlie05puth@gmail.com>
2023-09-10 18:36:24 +0530

    While loops

--- forLoop.dart
+++ forLoop.dart
@@ -2,4 +2,10 @@ void main() {
   for (int i = 0; i < 5; i++) {
     print("Hello");
   }
+
+  var list1 = [1, 2, 3, 4, 5];
+
+  for (var i in list1) {
+    print(i);
+  }
 }
---
+++ whileLoop.dart
@@ -0,0 +1,9 @@
+void main() {
+  int num = 0;
+  int maxNum = 10;
+
+  while (num < maxNum) {
+    print(num);
+    num += 1;
+  }
+}
